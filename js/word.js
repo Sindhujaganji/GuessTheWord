01417cf55c762c3758ddc85b3a209cef558f9fdf
Sindhuja Ganji <adepusindhuja1994@gmail.com>
2023-09-21 14:42:42 -0500

    first commit

---
+++ js/word.js
@@ -0,0 +1,51 @@
+const wordList = [
+    {
+        word: "air",
+        hint: "the invisible gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a small image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "python",
+        hint: "a scripting language"
+    },
+    {
+        word: "golang",
+        hint: "a programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet in solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    }
+
+]
